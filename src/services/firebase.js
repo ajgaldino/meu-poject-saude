d6cb11bb7fd1f8245edaa7ed39cb2f2ef34bd7ce
ajgaldino <annd18@live.com>
2025-05-25 11:06:55 -0300

    Start project

---
+++ src/services/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA48SCiTa6JtJFwc57hBot9loQNv2ocTGk",
+    authDomain: "project-saude-4c667.firebaseapp.com",
+    projectId: "project-saude-4c667",
+    storageBucket: "project-saude-4c667.firebasestorage.app",
+    messagingSenderId: "972417883246",
+    appId: "1:972417883246:web:dae079668fcd5a7cc5da7f",
+    measurementId: "G-28Z1ZFSH1P"
+
+};
+
+export const app = initializeApp(firebaseConfig);
